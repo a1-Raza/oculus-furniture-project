f89164359ce07be4bc607b2aefd8de3e7b785559
Abdurrahman Raza <abdurrahmanraza.ed@gmail.com>
2024-08-19 17:25:40 -0700

    test model registers ray interaction
    now to create a menu when you click on the object that will let you change between other objects

--- OculusV3/Assets/Scripts/TemplateObject.cs
+++ OculusV3/Assets/Scripts/TemplateObject.cs
@@ -1,31 +1,61 @@
+using Oculus.Interaction;
+using Oculus.Interaction.Surfaces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TemplateObject : MonoBehaviour
 {
+    [SerializeField] GameObject collideTestObject; // rename to menu or something when working on menu
+
     float initialTime;
+    bool modelRenderedSuccessfully;
+
+    MeshCollider modelCollider;
+    RayInteractable modelRayInteractable;
+    ColliderSurface modelColliderSurface;
 
     // Start is called before the first frame update
     void Start()
     {
+        collideTestObject.SetActive(false);
+        modelRenderedSuccessfully = false;
         initialTime = Time.time;
-        StartCoroutine(AddColliderToChild());
+        StartCoroutine(AddColliderToModelChild());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!modelRenderedSuccessfully) return;
+
+        if (modelRayInteractable.SelectingInteractors.Count > 0)
+        {
+            collideTestObject.SetActive(true);
+        }
+        else collideTestObject.SetActive(false);
     }
 
-    IEnumerator AddColliderToChild()
+    IEnumerator AddColliderToModelChild()
     {
-        yield return new WaitUntil(() => transform.childCount > 0 || Mathf.Abs(Time.time - initialTime) > 5f);
+        yield return new WaitUntil(() => transform.childCount > 1 || Mathf.Abs(Time.time - initialTime) > 5f);
         if (Mathf.Abs(Time.time - initialTime) > 5f) yield break;
 
         yield return new WaitForSecondsRealtime(0.25f);
-        GameObject child = transform.GetChild(0).gameObject;
-        child.AddComponent<MeshCollider>();
+        GameObject modelChild = transform.GetChild(1).gameObject;
+
+        modelChild.AddComponent<MeshCollider>();
+        modelChild.AddComponent<RayInteractable>();
+        modelChild.AddComponent<ColliderSurface>();
+
+        modelCollider = modelChild.GetComponent<MeshCollider>();
+        modelRayInteractable = modelChild.GetComponent<RayInteractable>();
+        modelColliderSurface = modelChild.GetComponent<ColliderSurface>();
+
+        modelColliderSurface.InjectCollider(modelCollider);
+        modelRayInteractable.InjectSurface(modelColliderSurface);
+
+        //collideTestObject = Instantiate(collideTestObject, gameObject.transform);
+        modelRenderedSuccessfully = true;
     }
 }
